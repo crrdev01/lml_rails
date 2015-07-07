da262b50fa02b3bb5fda1c2d78492c8d09158c9e
crrkit01 <crrkit01@gmail.com>
2015-07-06 23:45:49 -0400

    Change website to be positive

--- spec/features/stories_spec.rb
+++ spec/features/stories_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 feature 'Story views' do
-  scenario 'clicking your life sucks', js: true do
+  scenario 'clicking your life rocks', js: true do
     skip
     visit root_path
-    expect(page).to have_content 'I agree, your life sucks'
-    expect(first('.story')).to have_content "I agree, your life sucks"
+    expect(page).to have_content 'I agree, your life rocks'
+    expect(first('.story')).to have_content "I agree, your life rocks"
     first('.story > div > a').click
     sleep 1
     expect(first('.story > div > a')).to have_content "It definitely does!"
